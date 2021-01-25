093fd314e5c5bb2440212c83c8fac442ae122aeb
GitHub Action <action@github.com>
2021-01-25 12:17:57 +0000

    Audit Log Service commit hash: 996ecc9

--- git_push.sh
+++ git_push.sh
@@ -18,7 +18,7 @@ if [ "$git_repo_id" = "" ]; then
 fi
 
 if [ "$release_note" = "" ]; then
-    release_note="Audit Log Service commit hash: 98443bb"
+    release_note="Audit Log Service commit hash: 996ecc9"
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
 fi
 
